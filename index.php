ef3c3d474cf7893fc352459eb19460d227286ca5
lyhuu123 <lyhuu5570@gmail.com>
2023-11-25 00:55:34 +0700

    oke

--- index.php
+++ index.php
@@ -47,8 +47,6 @@
                 }else{
                     include_once "view/home.php";
                 }
-         
-
             break;
             case 'create_product':
                 include_once "view/create_product.php";
