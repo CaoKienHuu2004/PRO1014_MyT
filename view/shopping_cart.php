af26bc93f904919be74224953bef05d8abb10ae2
lyhuu123 <lyhuu5570@gmail.com>
2023-11-27 12:07:37 +0700

    ờm

--- view/shopping_cart.php
+++ view/shopping_cart.php
@@ -12,60 +12,7 @@
 //                     $tong+=$tt;
 //                     $tts=$_SESSION['giohang'][$i][3];
 //                     $tongs+=$tts;
-//                     $box_pr .= '<div class="lg-product-wrapper">
-//                     <div class="inner" >
-//                         <div class="lg-left-content" style="justify-content: center;">
-//                             <a href="product-details.php" class="thumbnail">
-//                                 <img src="view/layout/assets/images/portfolio/'.$_SESSION['giohang'][$i][0].'" alt="Nft_Portfolio">
-//                             </a>
-//                             <div class="read-content" style="max-width: 350px;">
-//                                 <a href="product-details.php">
-//                                     <h6 class="title">'.$_SESSION['giohang'][$i][1].'</h6>
-//                                 </a>
-//                                 <div class="product-share-wrapper">
-//                                     <!-- all bids -->
-//                                     <div class="profile-share">
-//                                         <a href="author.php" class="avatar" data-tooltip="@lyhuu123" tabindex="0"><img src="view/layout/assets/images/client/'.$_SESSION['giohang'][$i][0].'" alt="Nft_Profile"></a>
-                                        
-//                                         <a class="more-author-text" href="#" tabindex="0">Cao Kiến Hựu</a>
-//                                         <span style="color: #f27322; margin-left: 20px;"><b><i class="feather-check-circle"></i></b> đã được kiểm thử</span>
-//                                     </div>
-//                                     <!-- all bids End--> 
-                                    
-//                                 </div>
-//                                 <h6 class="latest-bid" style="color: #f27322;">'.$_SESSION['giohang'][$i][2].'</h6>
-//                                 <div class="share-wrapper d-flex">
-//                                     <!-- react area -->
-//                                     <!-- <div class="react-area mr--15">
-//                                         <span style="color: white; padding-left: 5px;">Xóa khỏi giỏ hàng</span>
-//                                     </div> -->
-//                                     <!-- end -->
-//                                     <!-- share area -->
-//                                     <div class="share-btn share-btn-activation dropdown">
-//                                         <button class="icon" data-bs-toggle="dropdown" aria-expanded="false" tabindex="0">
-//                                             <svg viewBox="0 0 14 4" fill="none" width="16" height="16" class="sc-bdnxRM sc-hKFxyN hOiKLt">
-//                                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M3.5 2C3.5 2.82843 2.82843 3.5 2 3.5C1.17157 3.5 0.5 2.82843 0.5 2C0.5 1.17157 1.17157 0.5 2 0.5C2.82843 0.5 3.5 1.17157 3.5 2ZM8.5 2C8.5 2.82843 7.82843 3.5 7 3.5C6.17157 3.5 5.5 2.82843 5.5 2C5.5 1.17157 6.17157 0.5 7 0.5C7.82843 0.5 8.5 1.17157 8.5 2ZM11.999 3.5C12.8274 3.5 13.499 2.82843 13.499 2C13.499 1.17157 12.8274 0.5 11.999 0.5C11.1706 0.5 10.499 1.17157 10.499 2C10.499 2.82843 11.1706 3.5 11.999 3.5Z" fill="currentColor"></path>
-//                                             </svg>
-//                                         </button>
-//                                         <div class="share-btn-setting dropdown-menu dropdown-menu-end">
-//                                             <button type="button" class="btn-setting-text share-text" data-bs-toggle="modal" data-bs-target="#shareModal" tabindex="0">
-//                                                 Share
-//                                             </button>
-//                                             <button type="button" class="btn-setting-text report-text" data-bs-toggle="modal" data-bs-target="#reportModal" tabindex="0">
-//                                                 Report
-//                                             </button>
-//                                         </div>
-//                                     </div>
-//                                     <!-- sharea End -->
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <div class="read-content" style="margin-left: 0px;">
-//                             <div class="share-wrapper d-flex">
-                                
-//                                 <div class="react-area mr--15" style="margin-right: 0px;">
-//                                     <span style="color: white;"><i class="feather-trash-2" href="index.php?pg=shopping-cart&delid=" style="color: white; padding-right: 5px;"></i>Xóa</span>
-//                                 </div>';
+//                     $box_pr .= '';
 
 //                 }
 //             $box_check .= '<div class="col-lg-4">
@@ -142,6 +89,60 @@
                             </li>
                             
                         </ul>
+                        <div class="lg-product-wrapper">
+//                     <div class="inner" >
+//                         <div class="lg-left-content" style="justify-content: center;">
+//                             <a href="product-details.php" class="thumbnail">
+//                                 <img src="view/layout/assets/images/portfolio/'.$_SESSION['giohang'][$i][0].'" alt="Nft_Portfolio">
+//                             </a>
+//                             <div class="read-content" style="max-width: 350px;">
+//                                 <a href="product-details.php">
+//                                     <h6 class="title">'.$_SESSION['giohang'][$i][1].'</h6>
+//                                 </a>
+//                                 <div class="product-share-wrapper">
+//                                     <!-- all bids -->
+//                                     <div class="profile-share">
+//                                         <a href="author.php" class="avatar" data-tooltip="@lyhuu123" tabindex="0"><img src="view/layout/assets/images/client/'.$_SESSION['giohang'][$i][0].'" alt="Nft_Profile"></a>
+                                        
+//                                         <a class="more-author-text" href="#" tabindex="0">Cao Kiến Hựu</a>
+//                                         <span style="color: #f27322; margin-left: 20px;"><b><i class="feather-check-circle"></i></b> đã được kiểm thử</span>
+//                                     </div>
+//                                     <!-- all bids End--> 
+                                    
+//                                 </div>
+//                                 <h6 class="latest-bid" style="color: #f27322;">'.$_SESSION['giohang'][$i][2].'</h6>
+//                                 <div class="share-wrapper d-flex">
+//                                     <!-- react area -->
+//                                     <!-- <div class="react-area mr--15">
+//                                         <span style="color: white; padding-left: 5px;">Xóa khỏi giỏ hàng</span>
+//                                     </div> -->
+//                                     <!-- end -->
+//                                     <!-- share area -->
+//                                     <div class="share-btn share-btn-activation dropdown">
+//                                         <button class="icon" data-bs-toggle="dropdown" aria-expanded="false" tabindex="0">
+//                                             <svg viewBox="0 0 14 4" fill="none" width="16" height="16" class="sc-bdnxRM sc-hKFxyN hOiKLt">
+//                                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M3.5 2C3.5 2.82843 2.82843 3.5 2 3.5C1.17157 3.5 0.5 2.82843 0.5 2C0.5 1.17157 1.17157 0.5 2 0.5C2.82843 0.5 3.5 1.17157 3.5 2ZM8.5 2C8.5 2.82843 7.82843 3.5 7 3.5C6.17157 3.5 5.5 2.82843 5.5 2C5.5 1.17157 6.17157 0.5 7 0.5C7.82843 0.5 8.5 1.17157 8.5 2ZM11.999 3.5C12.8274 3.5 13.499 2.82843 13.499 2C13.499 1.17157 12.8274 0.5 11.999 0.5C11.1706 0.5 10.499 1.17157 10.499 2C10.499 2.82843 11.1706 3.5 11.999 3.5Z" fill="currentColor"></path>
+//                                             </svg>
+//                                         </button>
+//                                         <div class="share-btn-setting dropdown-menu dropdown-menu-end">
+//                                             <button type="button" class="btn-setting-text share-text" data-bs-toggle="modal" data-bs-target="#shareModal" tabindex="0">
+//                                                 Share
+//                                             </button>
+//                                             <button type="button" class="btn-setting-text report-text" data-bs-toggle="modal" data-bs-target="#reportModal" tabindex="0">
+//                                                 Report
+//                                             </button>
+//                                         </div>
+//                                     </div>
+//                                     <!-- sharea End -->
+//                                 </div>
+//                             </div>
+//                         </div>
+//                         <div class="read-content" style="margin-left: 0px;">
+//                             <div class="share-wrapper d-flex">
+                                
+//                                 <div class="react-area mr--15" style="margin-right: 0px;">
+//                                     <span style="color: white;"><i class="feather-trash-2" href="index.php?pg=shopping-cart&delid=" style="color: white; padding-right: 5px;"></i>Xóa</span>
+//                                 </div>
                         <!-- tav Button area  End-->
                         
                         <!-- tab Content Start -->
