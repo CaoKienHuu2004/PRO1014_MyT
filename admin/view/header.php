d9ff5b60168e24c9786c8750d15847bf137af157
lyhuu123 <lyhuu5570@gmail.com>
2023-12-11 15:08:00 +0700

    Oke

--- admin/view/header.php
+++ admin/view/header.php
@@ -229,8 +229,8 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
             <a href="index3.html" class="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <img src="../view/layout/assets/images/logo/logo_MyT-01.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">Admin MyT</span>
             </a>
 
             <div class="sidebar">
@@ -240,7 +240,7 @@
                         <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">Quản Trị Viên</a>
                     </div>
                 </div>
 
